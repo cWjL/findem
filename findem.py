0bb98596a1340145bfc033793117eba607a1283a
cWjL <jacobloden@hotmail.com>
2019-08-19 14:47:58 -0700

    initial push

---
+++ findem.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+import whois
+import sys
+import argparse
+
+def main():
+    '''
+    Do main stuff
+    '''
+    parser = argparse.ArgumentParser()
+    reqd = parser.add_argument_group('required arguments')
+    reqd.add_argument('-t','--target',action='store',dest='tar',help='Keyword to find')
+    
+    args = parser.parse_args()
+    
+    sys.exit(0)
+    
+class effects(object):
+    '''
+    Terminal color object
+    '''
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    g_prefix = "["+GREEN+"*"+ENDC+"] "
+    b_prefix = "["+RED+"*"+ENDC+"] "
+    w_prefix = "["+YELLOW+"*"+ENDC+"] "
+    bl_prefix = "["+BLUE+"*"+ENDC+"] "
+    
+    def warn_msg(self, in_str):
+        '''
+        Warning message
+        @param String in_str
+        @return String yellow
+        '''
+        return self.YELLOW+in_str+self.ENDC
+    
+    def err_msg(self, in_str):
+        '''
+        Error message
+        @param String in_str
+        @return String red
+        '''
+        return self.RED+in_str+self.ENDC
+    
+    def good_msg(self, in_str):
+        '''
+        Good message
+        @param String in_str
+        @return String green
+        '''
+        return self.GREEN+in_str+self.ENDC
+
+    def blue_msg(self, in_str):
+        '''
+        Normal message
+        @param String in_str
+        @return String blue
+        '''
+        return self.BLUE+in_str+self.ENDC
+        
+if __name__ == "__main__":
+    main()
